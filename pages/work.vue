a5a1ff38871a8a3908d8b727ce4c14f4fd05fd66
suzuru0611 <suzuru0611@gmail.com>
2023-04-28 15:19:04 +0800

    04/28

--- pages/work.vue
+++ pages/work.vue
@@ -4,13 +4,15 @@
     Welcome to my Playground
   </div>
   <div>
-    <div class="text-[1.8rem] pb-2">這裡是什麼地方?
+    <div class="h-[40vh]">
+      <img src="" alt="">      
       <p class="text-[1rem] text-slate=300">WHAT?</p>
+      <a href="">more....</a>
     </div>
-    <div class="text-[1.8rem] pb-2">這裡存放了一些突發奇想的作品
-      主要都是由自已生活鎖事發想
-      <p class="text-[1rem] text-slate=300">(Hello, this is SUZURU Interest is learning,
-        It seems to know a lot of things, but it doesn't seem to be that good)</p>
-    </div>
+    <div class="h-[40vh]">
+      <img src="" alt="">      
+      <p class="text-[1rem] text-slate=300">WHAT?</p>
+      <a href="">more....</a>
+    </div>    
   </div>  
 </template>
