db8c48c9105da1cc9b95000e7bc118d08ccd1d15
suzuru0611 <suzuru0611@gmail.com>
2023-04-28 11:51:59 +0800

    04/28

--- components/navSpace.vue
+++ components/navSpace.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="w-full flex justify-center h-10 bg-stone-100">
-        <p class="text-xl ">Main Page</p>
-    </div>
-    <div>
-        <NuxtLink to="/about">
-            <p class="text">about</p>
-        </NuxtLink>
+    <div class="h-[5vh]">
+        <div class="w-full flex justify-center  bg-stone-100">
+            <p class="text-xl ">Main Page</p>
+        </div>
+        <div>
+            <NuxtLink to="/about">
+                <p class="text">about</p>
+            </NuxtLink>
+        </div> 
     </div>
 </template>
 <script>
--- pages/work.vue
+++ pages/work.vue
@@ -1,5 +1,8 @@
 <template>
   <NavSpace/> 
+  <div class="text-[10rem] pb-5  bg-stone-100">
+    Welcome to my Playground
+  </div>
   <div>
     <div class="text-[1.8rem] pb-2">這裡是什麼地方?
       <p class="text-[1rem] text-slate=300">WHAT?</p>
@@ -10,7 +13,4 @@
         It seems to know a lot of things, but it doesn't seem to be that good)</p>
     </div>
   </div>  
-  <div class="text-[10rem] pb-5">
-    ABOUT SUZURU
-  </div>
 </template>
