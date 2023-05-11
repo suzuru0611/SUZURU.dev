46cc53a22e72c059f68de0b8efb0baff006c7ffd
suzuru0611 <120362718+suzuru0611@users.noreply.github.com>
2023-05-11 22:23:13 +0800

    05/11

--- components/footerSpace.vue
+++ components/footerSpace.vue
@@ -1,8 +1,6 @@
 <template>
-<footer class="w-[100vw] flex justify-center items-center flex-col">
-  <div class=""><img src="../assets/icon.png" alt="icon"></div>
-
-  
+<footer class="w-[100vw] flex justify-center items-center flex-col py-2rem">
+  <div class="py-2rem"><img src="../assets/icon.png" alt="icon"></div>
   <div class=" flex-row flex justify-center">
     <div class="flex col justify-between w-[20rem] text-2xl">
       <NuxtLink to="/works" class="text-black no-underline hover:underline underline-offset-4">
@@ -16,6 +14,12 @@
       </NuxtLink>
     </div> 
   </div>
+  <div class="flex col justify-between w-[10rem] py-2rem">
+    <img class="w-[2rem]" src="../assets/github.png" alt="github">
+    <img class="w-[2rem]" src="../assets/linkin.png" alt="linkin">
+    <img class="w-[2rem]" src="../assets/behance.png" alt="behance">  
+  </div>
+  <p>SUZURU All Rights Reserved.</p>
 </footer>
 
 
