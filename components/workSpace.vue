888b9c03c252acff3c17a91589891c5a882badc3
suzuru0611 <120362718+suzuru0611@users.noreply.github.com>
2023-05-19 07:30:07 +0800

    05/19

--- components/workSpace.vue
+++ components/workSpace.vue
@@ -1,70 +1,65 @@
 <template>
-  <div class="bg-[#edeae5]  w-full ">
-    <P class="">作品</P>
-    <p>Works</p>
-   <section class="overflow-hidden">
-      <div class="grid grid-cols-1 gap-y-10 xl:grid-cols-3">
-        <div class="cardBg rounded-3xl justify-center z-10  xl:bg-white xl:justify-start 	">
-            <div class=" items-center absolute z-0 xl:relative">
-              <img class="flex  m-5  w-[45vw]  xl:w-[20vw]  opacity-30 xl:opacity-100" src="../assets/cdPlayer.gif" alt="Animated GIF">
-            </div>
-            <descript class="flex flex-col justify-center mb-8  break-all  z-10 h-[40rem] items-cemter xl:h-auto xl:items-start">
-              <strong class="text-[2rem] mb-3  xl:text-[2.2rem]  2xl:text-[2.4em] ">8 bits CDPlayer</strong>
-              <small class="text-[1rem] mb-3 xl:text-[1.2rem] 2xl:text-[1.4rem]">收藏了有關自製的8 BITs音樂</small>
-              <a href="https://www.youtube.com/watch?v=xhfJOkeBHy8" class="no-underline"><div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div></a>
-            </descript>
-        </div>
-        <div class="cardBg rounded-3xl justify-center z-10  xl:bg-white xl:justify-start ">
-            <div class=" items-center absolute z-0 xl:relative">
-              <img class="flex  m-5  w-[45vw]  xl:w-[20vw]  opacity-30 xl:opacity-100" src="../assets/phone.gif" alt="Animated GIF">
-            </div>
-            <descript class="flex flex-col justify-center mb-8  break-all  z-10 h-[40rem] items-center xl:h-auto  xl:items-start">
-              <strong class="text-[2rem] mb-3  xl:text-[2.2rem]  2xl:text-[2.4em] ">Weather Report</strong>
-              <small class="text-[1rem] mb-3 xl:text-[1.2rem] 2xl:text-[1.4rem]">查明天帶什麼出門</small>
-              <a href="https://www.youtube.com/watch?v=xhfJOkeBHy8" class="no-underline"><div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div></a>
-            </descript>
-        </div>
-        <div class="cardBg rounded-3xl justify-center z-10  xl:bg-white xl:justify-start ">
-            <div class=" items-center absolute z-0 xl:relative">
-              <img class="flex  m-5  w-[45vw]  xl:w-[20vw]  opacity-30 xl:opacity-100" src="../assets/suzuru.gif" alt="Animated GIF">
-            </div>
-            <descript class="flex flex-col justify-center mb-8  break-all  z-10 h-[40rem] items-center xl:h-auto xl:items-start">
-              <strong class="text-[2rem] mb-3  xl:text-[2.2rem]  2xl:text-[2.4em] ">SUZURU</strong>
-              <small class="text-[1rem] mb-3 xl:text-[1.2rem] 2xl:text-[1.4rem]">用於存放各個作品的網站</small>
-              <a href="https://www.youtube.com/watch?v=xhfJOkeBHy8" class="no-underline"><div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div></a>
-            </descript>
-        </div>
-        <div class="cardBg rounded-3xl justify-center z-10  xl:bg-white xl:justify-start ">
-            <div class=" items-center absolute z-0 xl:relative">
-              <img class="flex  m-5  w-[45vw]  xl:w-[20vw]  opacity-30 xl:opacity-100" src="../assets/squid.gif" alt="Animated GIF">
-            </div>
-            <descript class="flex flex-col justify-center mb-8  break-all  z-10 h-[40rem] items-center xl:h-auto xl:items-start">
-              <strong class="text-[2rem] mb-3  xl:text-[2.2rem]  2xl:text-[2.4em] ">S Squid</strong>
-              <small class="text-[1rem] mb-3 xl:text-[1.2rem] 2xl:text-[1.4rem]">紓壓用小魷魚</small>
-              <a href="https://www.youtube.com/watch?v=xhfJOkeBHy8" class="no-underline"><div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div></a>
-            </descript>
-        </div>
+  <div class="py-20">
+
+    <div class="flex w-full ">      
+      <div class="grid grid-cols-1 w-2/3 gap-y-10 xl:grid-row-3">
+       <div v-for="item in items" :key="item.id" class="cardBg rounded-3xl justify-center z-10  xl:bg-none xl:items-center ">
+           <descript class="flex flex-col justify-center mb-8 break-all z-10 h-[40rem] items-center xl:h-auto ">
+            <strong class="text-[2rem] mb-3 xl:text-[2.2rem] 2xl:text-[2.4em]">{{ item.title }}</strong>
+            <small class="text-[1rem] mb-3 xl:text-[1.2rem] 2xl:text-[1.4rem]">{{ item.description }}</small>
+            <a :href="item.link" class="no-underline">
+             <div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div>
+            </a>
+           </descript>
+       </div>
       </div>
-   </section> 
+      <div class=" rounded-3xl justify-end w-1/3 tracking-wide leading-loose text-right">
+        <div >
+          <p class="text-[6rem] text-slate-500 my-3rem">Works</p>
+        </div>
+         <descript class="flex flex-col mb-8 break-all z-10 h-[40rem]  items-center  xl:h-auto ">
+          <strong class="text-[2rem] mb-3 xl:text-[2.2rem] 2xl:text-[2.4em] ">作品都會集中在這,存放的種類並沒有特別限制,所以可以看到很不同類型的作品</strong>
+          <small class="text-[1rem] font-bold lg:text-[1.5rem] text-gray-700 " >( There are some whimsical works stored here,<br>which are inspired by life )</small>
+          <a class="no-underline">
+           <div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div>
+          </a>
+         </descript>
+      </div>
+
+    </div>
   </div>
 </template>
 <style > 
 
-.cardBg {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-left: 3rem;
-  margin-right: 3rem;
-  width: auto;
-  height: auto;
-  background-color: white;
-  transition: background-color 0.4s ease;
-}
-
-.cardBg:hover{
- background-color: #d9b61d;
-}
-
-
 </style>
+<script setup>
+import cdPlayerGif from '~/assets/cdPlayer.gif'
+import phoneGif from '~/assets/phone.gif'
+import suzuruGif from '~/assets/suzuru.gif'
+
+
+const items=[
+  {
+  id:1,
+  imgSrc:cdPlayerGif ,
+  imgAlt:"cdPlayerGif",
+  title:"8 bits CDPlayer",
+  link: "https://www.youtube.com/watch?v=xhfJOkeBHy8"
+  },
+  {
+  id:2,
+  imgSrc:phoneGif ,
+  imgAlt:"phoneGif",
+  title:"Weather Report",
+  link: "https://www.youtube.com/watch?v=xhfJOkeBHy8"
+  },
+  {
+  id:3,
+  imgSrc:suzuruGif ,
+  imgAlt:"suzuruGif",
+  title:"SUZURU",
+  link: "https://www.youtube.com/watch?v=xhfJOkeBHy8"
+  },
+]
+
+</script>
--- pages/works.vue
+++ pages/works.vue
@@ -3,59 +3,20 @@
    <NavSpace/> 
    <section class="overflow-hidden">
       <marquee/>
+
       <div class="grid grid-cols-1 gap-y-10 xl:grid-cols-2">
-        <div class="cardBg rounded-3xl justify-center z-10  xl:bg-white xl:justify-start 	">
-            <div class=" items-center absolute z-0 xl:relative">
-              <img class="flex  m-5  w-[45vw]  xl:w-[20vw]  opacity-30 xl:opacity-100" src="../assets/cdPlayer.gif" alt="Animated GIF">
-            </div>
-            <descript class="flex flex-col justify-center mb-8  break-all  z-10 h-[40rem] items-cemter xl:h-auto xl:items-start">
-              <strong class="text-[2rem] mb-3  xl:text-[2.2rem]  2xl:text-[2.4em] ">8 bits CDPlayer</strong>
-              <small class="text-[1rem] mb-3 xl:text-[1.2rem] 2xl:text-[1.4rem]">收藏了有關自製的8 BITs音樂</small>
-              <a href="https://www.youtube.com/watch?v=xhfJOkeBHy8" class="no-underline"><div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div></a>
-            </descript>
-        </div>
-        <div class="cardBg rounded-3xl justify-center z-10  xl:bg-white xl:justify-start ">
-            <div class=" items-center absolute z-0 xl:relative">
-              <img class="flex  m-5  w-[45vw]  xl:w-[20vw]  opacity-30 xl:opacity-100" src="../assets/phone.gif" alt="Animated GIF">
-            </div>
-            <descript class="flex flex-col justify-center mb-8  break-all  z-10 h-[40rem] items-center xl:h-auto  xl:items-start">
-              <strong class="text-[2rem] mb-3  xl:text-[2.2rem]  2xl:text-[2.4em] ">Weather Report</strong>
-              <small class="text-[1rem] mb-3 xl:text-[1.2rem] 2xl:text-[1.4rem]">查明天帶什麼出門</small>
-              <a href="https://www.youtube.com/watch?v=xhfJOkeBHy8" class="no-underline"><div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div></a>
-            </descript>
-        </div>
-        <div class="cardBg rounded-3xl justify-center z-10  xl:bg-white xl:justify-start ">
-            <div class=" items-center absolute z-0 xl:relative">
-              <img class="flex  m-5  w-[45vw]  xl:w-[20vw]  opacity-30 xl:opacity-100" src="../assets/suzuru.gif" alt="Animated GIF">
-            </div>
-            <descript class="flex flex-col justify-center mb-8  break-all  z-10 h-[40rem] items-center xl:h-auto xl:items-start">
-              <strong class="text-[2rem] mb-3  xl:text-[2.2rem]  2xl:text-[2.4em] ">SUZURU</strong>
-              <small class="text-[1rem] mb-3 xl:text-[1.2rem] 2xl:text-[1.4rem]">用於存放各個作品的網站</small>
-              <a href="https://www.youtube.com/watch?v=xhfJOkeBHy8" class="no-underline"><div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div></a>
-            </descript>
-        </div>
-        <div class="cardBg rounded-3xl justify-center z-10  xl:bg-white xl:justify-start ">
-            <div class=" items-center absolute z-0 xl:relative">
-              <img class="flex  m-5  w-[45vw]  xl:w-[20vw]  opacity-30 xl:opacity-100" src="../assets/squid.gif" alt="Animated GIF">
-            </div>
-            <descript class="flex flex-col justify-center mb-8  break-all  z-10 h-[40rem] items-center xl:h-auto xl:items-start">
-              <strong class="text-[2rem] mb-3  xl:text-[2.2rem]  2xl:text-[2.4em] ">S Squid</strong>
-              <small class="text-[1rem] mb-3 xl:text-[1.2rem] 2xl:text-[1.4rem]">紓壓用小魷魚</small>
-              <a href="https://www.youtube.com/watch?v=xhfJOkeBHy8" class="no-underline"><div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div></a>
-            </descript>
-        </div>
-      </div>
-      <div v-for="item in items" :key="item.id" class="cardBg rounded-3xl justify-center z-10  xl:bg-white xl:justify-start ">
-        <div class="items-center absolute z-0 xl:relative">
-          <img class="flex m-5 w-[45vw] xl:w-[20vw] opacity-30 xl:opacity-100" :src="item.imgSrc" :alt="item.imgAlt">
-        </div>
-        <descript class="flex flex-col justify-center mb-8 break-all z-10 h-[40rem] items-center xl:h-auto xl:items-start">
-         <strong class="text-[2rem] mb-3 xl:text-[2.2rem] 2xl:text-[2.4em]">{{ item.title }}</strong>
-         <small class="text-[1rem] mb-3 xl:text-[1.2rem] 2xl:text-[1.4rem]">{{ item.description }}</small>
-         <a :href="item.link" class="no-underline">
-          <div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div>
-         </a>
-        </descript>
+       <div v-for="item in items" :key="item.id" class="cardBg rounded-3xl justify-center z-10  xl:bg-white xl:justify-start ">
+         <div class="items-center absolute z-0 xl:relative">
+           <img class="flex m-5 w-[45vw] xl:w-[20vw] opacity-30 xl:opacity-100" :src="item.imgSrc" :alt="item.imgAlt">
+         </div>
+         <descript class="flex flex-col justify-center mb-8 break-all z-10 h-[40rem] items-center xl:h-auto xl:items-start">
+          <strong class="text-[2rem] mb-3 xl:text-[2.2rem] 2xl:text-[2.4em]">{{ item.title }}</strong>
+          <small class="text-[1rem] mb-3 xl:text-[1.2rem] 2xl:text-[1.4rem]">{{ item.description }}</small>
+          <a :href="item.link" class="no-underline">
+           <div class="border-2 border-slate border-solid justify-center items-center flex rounded-xl w-[12rem] h-[3rem] text-[1.3rem] text-black hover:animate-pulse">More...</div>
+          </a>
+         </descript>
+       </div>
       </div>
       <marquee/>
    </section> 
@@ -80,15 +41,45 @@
 }
 </style>
 <script setup>
+import cdPlayerGif from '~/assets/cdPlayer.gif'
+import phoneGif from '~/assets/phone.gif'
+import suzuruGif from '~/assets/suzuru.gif'
+import squidGif from '~/assets/squid.gif'
+
+
 const items=[
   {
   id:1,
-  imgSrc:"~/assets/cdPlayer.gif",
-  imgAlt:"Asadf",
+  imgSrc:cdPlayerGif ,
+  imgAlt:"cdPlayerGif",
+  title:"8 bits CDPlayer",
+  description: "收藏了有關自製的8 BITs音樂",
+  link: "https://www.youtube.com/watch?v=xhfJOkeBHy8"
+  },
+  {
+  id:2,
+  imgSrc:phoneGif ,
+  imgAlt:"phoneGif",
+  title:"Weather Report",
+  description: "查明天帶什麼出門",
+  link: "https://www.youtube.com/watch?v=xhfJOkeBHy8"
+  },
+  {
+  id:3,
+  imgSrc:suzuruGif ,
+  imgAlt:"suzuruGif",
+  title:"SUZURU",
+  description: "用於存放各個作品的網站",
+  link: "https://www.youtube.com/watch?v=xhfJOkeBHy8"
+  },
+  {
+  id:4,
+  imgSrc:squidGif ,
+  imgAlt:"squidGif",
   title:"S Squid",
   description: "紓壓用小魷魚",
   link: "https://www.youtube.com/watch?v=xhfJOkeBHy8"
-  }
+  },
 ]
 
 </script>
