4c4e6b6deacdd45ff026eac6fd77d874aa159fa2
suzuru0611 <120362718+suzuru0611@users.noreply.github.com>
2023-10-04 15:27:07 +0800

    change main gif

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,27 @@
 <template>
   <div class="bg-[#edeae5]  w-full ">
-    <navSpace/>
+    <navSpace />
     <section class="relative flex flex-col h-auto mx-2rem md:mx-10rem 2xl:h-[65rem]">
       <div class="pt-5rem  tracking-wide leading-loose">
         <p class="text-[4rem] text-slate-500 lg:text-[4.5rem] ">This Is My Thought Locker </p>
         <P class="text-[2rem] font-bold lg:text-[2.5rem]">這裡存放了一些突發奇想的作品、<br>由生活鎖事去發想</P>
-        <P class="text-[1rem] font-bold lg:text-[1.5rem] text-gray-700 " >( There are some whimsical works stored here,<br>which are inspired by life )</P>
+        <P class="text-[1rem] font-bold lg:text-[1.5rem] text-gray-700 ">( There are some whimsical works stored
+          here,<br>which are inspired by life )</P>
       </div>
       <div class="flex w-full justify-end relative  2xl:absolute">
-        <img class="flex w-[42vw] " src="../assets/indexComputer.gif" alt="Animated GIF">
+        <img class="flex w-[40vw] " src="../assets/indexComputer.gif" alt="Animated GIF">
       </div>
     </section>
-    <aboutSpace/>
-    <WorkSpace/>
+    <aboutSpace />
+    <WorkSpace />
   </div>
-  <FooterSpace/>
+  <FooterSpace />
 </template>
-<style > 
-html, body {
-  margin: 0px !important;
-  padding: 0px !important;
-}
+<style > html,
+ body {
+   margin: 0px !important;
+   padding: 0px !important;
+ }
 </style>
 
 
